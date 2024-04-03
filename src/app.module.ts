ea7005a5ea2a585abd45af423b8637a8f2b79721
Made Agus Andi Gunawan <madeagusandi446@gmail.com>
2024-04-03 21:57:16 +0700

    fix(app-module): remove and add database secret to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule, 
-    MongooseModule.forRoot('mongodb+srv://user_workflow:infomediatestingPoint@bpmplatform.hkm2gp9.mongodb.net/?retryWrites=true&w=majority&appName=bpmplatform')],
+  imports: [
+    ProductsModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      useFactory: async () => ({
+        uri: process.env.DATABASE_URL,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
